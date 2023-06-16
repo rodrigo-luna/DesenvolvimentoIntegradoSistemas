87b70df14060cd7a942cb8b024bf432efb905e85
Rodrigo Luna <rlunaftw@gmail.com>
2023-06-16 18:07:14 -0300

    JSON

--- Servidor/main.py
+++ Servidor/main.py
@@ -7,18 +7,30 @@ import os
 from _thread import *
 import random
 import time
+import json
 
-def multi_threaded_client(connection):
+# ==================================================
+
+def atendeClient(connection, address):
 	connection.send(str.encode('Server is working:'))
 	while True:
 		data = connection.recv(2048)
 
-		time.sleep(1 + random.random()*4)
-
-		response = 'O servidor terminou de atender: ' + data.decode('utf-8')
-
 		if not data:
 			break
+
+		dataJSON = json.loads(data.decode('utf-8'))
+		addressJSON = { "address" : address[0] + ':' + str(address[1]) }
+		dataJSON.update(addressJSON)
+
+		file = open("log.txt", "a")
+		file.write(str(dataJSON) + ',\n')
+		file.close()
+
+		response = "Recebi a matriz do cliente " + dataJSON["id"] + ' (endereço ' + dataJSON["address"] + ')'
+
+		time.sleep(1 + random.random()*4)
+
 		connection.sendall(str.encode(response))
 	connection.close()
 
@@ -26,7 +38,7 @@ def main():
 	ServerSideSocket = socket.socket()
 	host = '127.0.0.1'
 	port = 2004
-	ThreadCount = 0
+	threadCount = 0
 	try:
 		ServerSideSocket.bind((host, port))
 	except socket.error as e:
@@ -34,12 +46,16 @@ def main():
 	print('Socket is listening..')
 	ServerSideSocket.listen(5)
 
+	file = open("log.txt", "w")
+	file.write('')
+	file.close()
+
 	while True:
 		Client, address = ServerSideSocket.accept()
 		print('Connected to: ' + address[0] + ':' + str(address[1]))
-		start_new_thread(multi_threaded_client, (Client, ))
-		ThreadCount += 1
-		print('Thread Number: ' + str(ThreadCount))
+		start_new_thread(atendeClient, (Client, address,))
+		threadCount += 1
+		print('Thread Number: ' + str(threadCount))
 	ServerSideSocket.close()
 
 if __name__ == '__main__':
